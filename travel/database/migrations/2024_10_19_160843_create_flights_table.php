98b4623efd33ca1c0e14ca06a774ac1c8f719847
karsaibence <karesz333@hotmail.com>
2024-10-19 18:42:46 +0200

    alap, de meg hibas

---
+++ travel/database/migrations/2024_10_19_160843_create_flights_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('flights', function (Blueprint $table) {
+            $table->id('flight_id')->primary();
+            $table->date('date');
+            $table->foreignId('airline_id');
+            $table->integer('limit');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('flights');
+    }
+};
---
+++ travel/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Airlines;
+use App\Models\Flight;
+use App\Models\Travel;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+         User::factory(10)->create();
+         Airlines::factory(10)->create();
+         Flight::factory(10)->create();
+         Travel::factory(10)->create();
+
+        /*User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);*/
+    }
+}
